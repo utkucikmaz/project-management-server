2223197cb41a0055cefb4c119fad6cba063c7477
Utku Cikmaz <utkucikmaz@gmail.com>
2023-09-19 11:43:34 +0200

    handling error messages

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+
 const Task = require("../models/Task.model");
 const Project = require("../models/Project.model");
 
 //  POST /api/tasks  -  Creates a new task
-router.post("/tasks", (req, res, next) => {
+router.post("/tasks", isAuthenticated, (req, res, next) => {
     const { title, description, projectId } = req.body;
 
     const newTask = {
@@ -21,7 +23,13 @@ router.post("/tasks", (req, res, next) => {
             });
         })
         .then((response) => res.json(response))
-        .catch((err) => res.json(err));
+        .catch((err) => {
+            console.log("Error creating new task...", err);
+            res.status(500).json({
+                message: "Error creating a new task",
+                error: err,
+            });
+        });
 });
 
 module.exports = router;
